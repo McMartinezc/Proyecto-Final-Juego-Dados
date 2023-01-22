a7ed17d5d9c4d7449a38ea4bdf1bdecfb71618c8
McMartinezc <105592833+McMartinezc@users.noreply.github.com>
2023-01-22 11:51:10 +0100

    Delete jocDeDaus - jwt-sql - roles directory

--- jocDeDaus - jwt-sql - roles/src/main/java/com/jocdedaus/MartinezMCarmen/T05S02MartinezMCarmen/Segurity/service/RoleService.java
+++
@@ -1,7 +0,0 @@
-package com.jocdedaus.MartinezMCarmen.T05S02MartinezMCarmen.Segurity.service;
-
-import com.jocdedaus.MartinezMCarmen.T05S02MartinezMCarmen.Segurity.entities.Role;
-
-public interface RoleService {
-    Role findByName(String name);
-}
