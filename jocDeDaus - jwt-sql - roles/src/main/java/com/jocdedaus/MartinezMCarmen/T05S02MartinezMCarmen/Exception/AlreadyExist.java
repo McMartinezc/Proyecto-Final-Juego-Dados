c1385dc542884cc520c5574b2cb42df59ce0869a
McMartinezc <105592833+McMartinezc@users.noreply.github.com>
2023-01-22 11:46:27 +0100

    Delete jocDeDaus - jwt-sql - roles directory

--- jocDeDaus - jwt-sql - roles/src/main/java/com/jocdedaus/MartinezMCarmen/T05S02MartinezMCarmen/Exception/AlreadyExist.java
+++
@@ -1,7 +0,0 @@
-package com.jocdedaus.MartinezMCarmen.T05S02MartinezMCarmen.Exception;
-
-public class AlreadyExist extends  RuntimeException{
-    public AlreadyExist(String message) {
-        super(message);
-    }
-}
